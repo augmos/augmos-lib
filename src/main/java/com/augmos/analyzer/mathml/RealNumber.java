c5a672b58c59b692b2620b32cd3e79dbc60d2535
Erik Lüth <erik.lueth@tum.de>
2019-05-25 19:47:14 +0200

    RealNumber implementation complete

--- src/main/java/com/augmos/analyzer/mathml/RealNumber.java
+++ src/main/java/com/augmos/analyzer/mathml/RealNumber.java
@@ -11,14 +11,6 @@ public class RealNumber {
         this.realNumberNotation = realNumberNotation;
     }
 
-    static private int gcd(int a, int b) {
-        while (a != b) {
-            if (a > b) a -= b;
-            else b -= a;
-        }
-        return a;
-    }
-
     public RealNumber add(RealNumber arg){
         int kgv = this.divisor * arg.divisor / gcd(this.divisor, arg.divisor);
         int div1 = this.dividend * (kgv / this.divisor);
@@ -28,57 +20,43 @@ public class RealNumber {
         newDiv = newDiv / gcd(newDiv, kgv);
         int newBasis = kgv / gcd(newDiv, kgv);
 
-        //Check if Integer and return
-        if(newDiv % newBasis == 0){
-            return new RealNumber(newDiv / newBasis, 1, RealNumberNotation.INTEGER);
-        }
-        //2,4,5,10 als basis zu float
-        if(newBasis == 2 || newBasis == 4 || newBasis == 5 || newBasis % 10 == 0){
-            return new RealNumber(newDiv, newBasis, RealNumberNotation.FLOAT);
-        }
-
-        return new RealNumber(newDiv, newBasis, RealNumberNotation.FRACTION);
-
+        return setNewNotation(newDiv, newBasis);
     }
 
     public RealNumber sub(RealNumber arg){
-        int kgv = this.divisor * arg.divisor / gcd(this.divisor, arg.divisor);
-        int div1 = this.dividend * (kgv / this.divisor);
-        int div2 = arg.dividend * (kgv / arg.divisor);
-        int newDiv = div1 - div2;
-        // Get smallest basis
-        newDiv = newDiv / gcd(newDiv, kgv);
-        int newBasis = kgv / gcd(newDiv, kgv);
+        //dividend negiert
+        return this.add(new RealNumber(-arg.dividend, arg.divisor, arg.realNumberNotation));
 
-        //Check if Integer and return
-        if(newDiv % newBasis == 0){
-            return new RealNumber(newDiv / newBasis, 1, RealNumberNotation.INTEGER);
+    }
+
+    public RealNumber div(RealNumber arg) {
+        //argumente umgedreht
+        return this.mul(new RealNumber(arg.divisor, arg.dividend, arg.realNumberNotation));
+    }
+
+    public RealNumber mul(RealNumber arg) {
+        return setNewNotation(this.dividend * arg.dividend, this.divisor * arg.divisor);
+    }
+
+    public int compareTo(RealNumber arg) {
+        double curr = this.dividend / this.divisor;
+        double argD = arg.dividend / arg.divisor;
+        if (curr == argD) {
+            return 0;
+        } else if (curr < argD) {
+            return -1;
         }
-        //2,4,5,10 als basis zu float
-        if(newBasis == 2 || newBasis == 4 || newBasis == 5 || newBasis % 10 == 0){
-            return new RealNumber(newDiv, newBasis, RealNumberNotation.FLOAT);
-        }
-
-        return new RealNumber(newDiv, newBasis, RealNumberNotation.FRACTION);
-
+        return 1;
     }
-    public RealNumber div(RealNumber arg){
-
-    }
-    public RealNumber mul(RealNumber arg){
-
-    }
-    public int compareTo(RealNumber arg){
-
-    }
-
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
         if (obj == null || getClass() != obj.getClass()) return false;
         final RealNumber that = (RealNumber) obj;
-        //TODO
-        return super.equals(obj);
+        int kgv = this.divisor * that.divisor / gcd(this.divisor, that.divisor);
+        int div1 = this.dividend * (kgv / this.divisor);
+        int div2 = that.dividend * (kgv / that.divisor);
+        return div1 == div2;
     }
 
     @Override
@@ -86,9 +64,33 @@ public class RealNumber {
         switch (realNumberNotation){
             case FLOAT: return Float.toString(dividend / (float) divisor);
             case INTEGER: return Integer.toString(dividend);
-            case FRACTION: return Integer.toString(dividend) + " / " + Integer.toString(divisor); //TODO später ggf anpassen
+            case FRACTION:
+                return dividend + " / " + divisor; //TODO später ggf anpassen
         }
         return super.toString();
+    }
+
+    //private helping methods
+
+    static private int gcd(int a, int b) {
+        while (a != b) {
+            if (a > b) a -= b;
+            else b -= a;
+        }
+        return a;
+    }
+
+    static private RealNumber setNewNotation(int div, int base) {
+        //Check if Integer and return
+        if (div % base == 0) {
+            return new RealNumber(div / base, 1, RealNumberNotation.INTEGER);
+        }
+        //2,4,5,10 als basis zu float
+        if (base == 2 || base == 4 || base == 5 || base % 10 == 0) {
+            return new RealNumber(div, base, RealNumberNotation.FLOAT);
+        }
+
+        return new RealNumber(div, base, RealNumberNotation.FRACTION);
     }
 
     enum RealNumberNotation{
